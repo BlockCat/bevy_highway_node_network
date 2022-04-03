e130f25024ef87ff17a3dff4e6681b739ace73f8
BlockCat <zinoonomiwo@gmail.com>
2022-04-03 23:23:14 +0200

    enable ? test

--- network/src/neighbourhood.rs
+++ network/src/neighbourhood.rs
@@ -91,81 +91,34 @@ fn find_backward_neighbourhood_radius<D: NetworkData>(
         .map(|x| x.1)
 }
 
-// #[cfg(test)]
-// mod tests {
+#[cfg(test)]
+mod tests {
 
-//     use crate::{
-//         create_network,
-//         tests::{TestEdge, TestNode},
-//         DirectedNetworkGraph, EdgeId, ForwardNeighbourhood, NodeId, PathParent,
-//     };
+    use crate::{
+        create_network,
+        tests::{TestEdge, TestNode},
+        ForwardNeighbourhood,
+    };
 
-//     #[test]
-//     fn forward_neighbourhood_test() {
-//         // https://www.baeldung.com/wp-content/uploads/2017/01/initial-graph.png
-//         let network = create_network!(
-//             0 => 1; 10.0,
-//             0 => 2; 15.0,
-//             1 => 3; 5.0,
-//             1 => 5; 15.0,
-//             2 => 4; 10.0,
-//             3 => 4; 2.0,
-//             3 => 5; 1.0,
-//             5 => 4; 5.0
-//         );
+    #[test]
+    fn forward_neighbourhood_test() {
+        // https://www.baeldung.com/wp-content/uploads/2017/01/initial-graph.png
+        let network = create_network!(
+            0..5,
+            0 => 1; 10.0,
+            0 => 2; 15.0,
+            1 => 3; 5.0,
+            1 => 5; 15.0,
+            2 => 4; 10.0,
+            3 => 4; 2.0,
+            3 => 5; 1.0,
+            5 => 4; 5.0
+        );
 
-//         let forward = ForwardNeighbourhood::from_network(3, &network);
+        let forward = ForwardNeighbourhood::from_network(3, &network);
 
-//         let radius = &forward.radius;
+        let radius = &forward.radius;
 
-//         assert_eq!(&vec![15.0, 6.0, 10.0, 2.0, 0.0, 5.0], radius);
-
-//         // let network = &forward.neighbours;
-
-//         // assert_eq!(
-//         //     HashMap::from([
-//         //         (NodeId(0), None),
-//         //         (NodeId(1), Some(PathParent::new(10.0, NodeId(0)))),
-//         //         (NodeId(2), Some(PathParent::new(15.0, NodeId(0)))),
-//         //         (NodeId(3), Some(PathParent::new(15.0, NodeId(1)))),
-//         //     ]),
-//         //     network[0]
-//         // );
-
-//         // assert_eq!(
-//         //     HashMap::from([
-//         //         (NodeId(1), None),
-//         //         (NodeId(3), Some(PathParent::new(5.0, NodeId(1)))),
-//         //         (NodeId(5), Some(PathParent::new(6.0, NodeId(3)))),
-//         //     ]),
-//         //     network[1]
-//         // );
-
-//         // assert_eq!(
-//         //     HashMap::from([
-//         //         (NodeId(2), None),
-//         //         (NodeId(4), Some(PathParent::new(10.0, NodeId(2)))),
-//         //     ]),
-//         //     network[2]
-//         // );
-
-//         // assert_eq!(
-//         //     HashMap::from([
-//         //         (NodeId(3), None),
-//         //         (NodeId(4), Some(PathParent::new(2.0, NodeId(3)))),
-//         //         (NodeId(5), Some(PathParent::new(1.0, NodeId(3)))),
-//         //     ]),
-//         //     network[3]
-//         // );
-
-//         // assert_eq!(HashMap::from([(NodeId(4), None),]), network[4]);
-
-//         // assert_eq!(
-//         //     HashMap::from([
-//         //         (NodeId(5), None),
-//         //         (NodeId(4), Some(PathParent::new(5.0, NodeId(5)))),
-//         //     ]),
-//         //     network[5]
-//         // );
-//     }
-// }
+        assert_eq!(&vec![15.0, 6.0, 10.0, 2.0, 0.0, 5.0], radius);
+    }
+}
